c0b6fc4b32b240cda0797ca8a5305e646c0ddeb6
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-11-10 20:09:58 -0300

    feat/ created dev run script

--- src/services/phone-store-api.js
+++ src/services/phone-store-api.js
@@ -1,7 +1,10 @@
 /* eslint-disable */
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL =
+  process.env.NODE_ENV === 'dev'
+    ? 'http://localhost:4000'
+    : 'https://phone-store-driven.herokuapp.com';
 
 function getProducts() {
   const promise = axios.get(`${BASE_URL}/products`);
