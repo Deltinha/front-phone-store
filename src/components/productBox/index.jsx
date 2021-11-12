e52d8e42f2af4afd0b0e9bdf36436d04dfbc263c
coelhothay <thay.csb@gmail.com>
2021-11-12 18:53:04 -0300

    fix/ color name in the product page

--- src/components/productBox/index.jsx
+++ src/components/productBox/index.jsx
@@ -1,19 +1,29 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './style';
+import getColorName from '../../services/color-name-api';
 
 export default function ProductBox({ product }) {
+  const [colorName, setColorName] = useState('');
+
   const {
     model, capacity, value, description, color, brand, productImages,
   } = product;
 
   const firstImage = productImages.find((img) => img.perspective === 'front');
 
+  useEffect(() => {
+    getColorName(color)
+      .then((res) => {
+        setColorName(res.data?.colors[0].name);
+      });
+  }, []);
+
   return (
     <S.ProductDetail>
       <S.ProductImage src={firstImage.url} />
       <div>
-        {(model) ? <S.Title>{`${model} ${capacity || ''} ${color || ''}`}</S.Title> : ''}
+        {(model) ? <S.Title>{`${model} ${capacity || ''} ${colorName || ''}`}</S.Title> : ''}
         {(value) ? <h3>{`R$ ${(value / 100).toFixed(2)}`}</h3> : ''}
         <p>{`Em até 10x de R$ ${(value / 1000).toFixed(2)} sem juros`}</p>
         <p>{description}</p>
