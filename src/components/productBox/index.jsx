293104c82bd1058e51bbc7ce80d63c069c6b41e8
coelhothay <thay.csb@gmail.com>
2021-11-12 01:09:55 -0300

    fix/ validate product info to render

--- src/components/productBox/index.jsx
+++ src/components/productBox/index.jsx
@@ -4,7 +4,7 @@ import * as S from './style';
 
 export default function ProductBox({ product }) {
   const {
-    model, capacity, color, value, description, brand, productImages,
+    model, capacity, value, description, color, brand, productImages,
   } = product;
 
   const firstImage = productImages.find((img) => img.perspective === 'front');
@@ -13,14 +13,14 @@ export default function ProductBox({ product }) {
     <S.ProductDetail>
       <S.ProductImage src={firstImage.url} />
       <div>
-        <S.Title>{`${model} ${capacity} ${color}`}</S.Title>
-        <h3>{`R$ ${(value / 100).toFixed(2)}`}</h3>
+        {(model) ? <S.Title>{`${model} ${capacity || ''} ${color || ''}`}</S.Title> : ''}
+        {(value) ? <h3>{`R$ ${(value / 100).toFixed(2)}`}</h3> : ''}
         <p>{`Em até 10x de R$ ${(value / 1000).toFixed(2)} sem juros`}</p>
         <p>{description}</p>
-        <p>{`Marca: ${brand}`}</p>
+        {(brand) ? <p>{`Marca: ${brand}`}</p> : ''}
         <p>Tipo de produto: Smartphone</p>
-        <p>{` Capacidade de armazenamento da memória: ${capacity}`}</p>
-        <p>{`Cor: ${color}`}</p>
+        {(capacity) ? <p>{` Capacidade de armazenamento da memória: ${capacity}`}</p> : ''}
+        {(color) ? <p>{`Cor: ${color}`}</p> : ''}
       </div>
     </S.ProductDetail>
   );
