945c1e8324286167129ad0e15902c96e3991268c
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-11-14 17:58:05 -0300

    feat/ sucess alert on checkout

--- src/components/CheckoutButton/index.jsx
+++ src/components/CheckoutButton/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { RiTruckFill } from 'react-icons/ri';
+import Swal from 'sweetalert2';
 import * as S from './style';
+
+function successAlert() {
+  Swal.fire(
+    'Tudo certo!',
+    'Compra finalizada!',
+    'success',
+  );
+}
 
 export default function CheckoutButton() {
   return (
-    <S.CheckoutButton>
+    <S.CheckoutButton onClick={() => successAlert()}>
       <S.CheckoutIconWrapper>
         <RiTruckFill />
       </S.CheckoutIconWrapper>
