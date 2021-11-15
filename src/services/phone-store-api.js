77a789e7e5a92fb38fab9e6844bdd349950847c9
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-11-15 00:21:57 -0300

    feat/ post checkout api function

--- src/services/phone-store-api.js
+++ src/services/phone-store-api.js
@@ -4,7 +4,14 @@ const BASE_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:4000'
   : 'https://phone-store-driven.herokuapp.com';
 
-function getProducts() {
+function createHeaders(token) {
+  const headers = {
+    Authentication: `Bearer ${token}`,
+  };
+  return headers;
+}
+
+export function getProducts() {
   const promise = axios.get(`${BASE_URL}/products`);
   return promise;
 }
@@ -15,12 +22,16 @@ export function getProductsByCategorie({ categorie, value }) {
   return promise;
 }
 
-function getProductById(id) {
+export function getProductById(id) {
   return axios.get(`${BASE_URL}/products/${id}`);
 }
 
-function getCategories() {
+export function getCategories() {
   return axios.get(`${BASE_URL}/categories`);
 }
 
-export { getProducts, getProductById, getCategories };
+export function postCheckout({ body, token }) {
+  const headers = createHeaders(token);
+  const promise = axios.post(`${BASE_URL}/checkout`, body, headers);
+  return promise;
+}
